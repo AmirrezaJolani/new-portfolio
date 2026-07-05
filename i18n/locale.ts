ba11357a93444a5b2de2d415f5a7e2970e1e5f8c
AmirrezaJolani <amirreza.mameghani@pillar.it>
2026-07-05 22:48:43 +0200

    feat: add cookie-based next-intl i18n (en + fa/rtl, no url locale)

---
+++ i18n/locale.ts
@@ -0,0 +1,16 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { hasLocale } from "next-intl";
+import { defaultLocale, locales, type Locale } from "./config";
+
+const COOKIE_NAME = "NEXT_LOCALE";
+
+export async function getUserLocale(): Promise<Locale> {
+  const value = (await cookies()).get(COOKIE_NAME)?.value;
+  return hasLocale(locales, value) ? value : defaultLocale;
+}
+
+export async function setUserLocale(locale: Locale) {
+  (await cookies()).set(COOKIE_NAME, locale);
+}
